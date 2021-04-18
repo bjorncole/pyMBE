fee179892af78e72698c4ba2b76b3635d940cd0a
Santiago <sanbales@gmail.com>
2021-04-17 21:03:32 -0400

    Made changes to query.py as per Bjorn's request

--- src/pymbe/query/query.py
+++ src/pymbe/query/query.py
@@ -1,75 +1,73 @@
 # a set of queries to run on Labeled Property Graphs
 
-import networkx as NX
+import networkx as nx
 import math
-from ..graph.filter_lib import expanded_banded_graph_filter
-from ..client import SysML2Client
+from ..graph.lpg import SysML2LabeledPropertyGraph
 from .metamodel_navigator import *
 
 
-def roll_up_lower_multiplicity(lpg: NX.MultiDiGraph, feat: dict, client: SysML2Client) -> int:
+def roll_up_lower_multiplicity(
+    lpg: SysML2LabeledPropertyGraph,
+    feat: dict,
+) -> int:
 
-    bgf = expanded_banded_graph_filter()
-
-    banded_featuring_graph = lpg.filter(
-        edge_types=bgf['edge_types'],
-        reverse_edge_types=bgf['reverse_edge_types']
-    )
-
-    banded_roots = [node
-                    for node in banded_featuring_graph.nodes
-                    if banded_featuring_graph.out_degree(node) == 0]
+    banded_featuring_graph = lpg.get_projection("expanded_banded_graph")
+    banded_roots = [
+        node
+        for node in banded_featuring_graph.nodes
+        if banded_featuring_graph.out_degree(node) < 1
+    ]
 
     corrected_mult = 1
-
     for part_tree_root in banded_roots:
         try:
-            part_path = NX.shortest_path(
+            part_path = nx.shortest_path(
                 banded_featuring_graph,
-                feat['@id'],
-                part_tree_root)
+                source=feat['@id'],
+                target=part_tree_root,
+            )
             # TODO: check that the path actually exists
-            corrected_mult = math.prod(
-                [feature_lower_multiplicity(client.elements_by_id[node], client)
-                 for node in part_path])
-        except NX.NetworkXNoPath:
+            corrected_mult = math.prod([
+                feature_lower_multiplicity(lpg.elements_by_id[node], lpg)
+                for node in part_path
+            ])
+        except nx.NetworkXNoPath:
             pass
-        except NX.NodeNotFound:
+        except nx.NodeNotFound:
             # nothing to roll up, so just use own multiplicity
-            corrected_mult = feature_lower_multiplicity(feat, client)
+            corrected_mult = feature_lower_multiplicity(feat, lpg)
 
     return corrected_mult
 
 
-def roll_up_upper_multiplicity(lpg: NX.MultiDiGraph, feat: dict, client: SysML2Client) -> int:
-
-    bgf = expanded_banded_graph_filter()
-
-    banded_featuring_graph = lpg.filter(
-        edge_types=bgf['edge_types'],
-        reverse_edge_types=bgf['reverse_edge_types']
-    )
-
-    banded_roots = [node
-                    for node in banded_featuring_graph.nodes
-                    if banded_featuring_graph.out_degree(node) == 0]
+def roll_up_upper_multiplicity(
+    lpg: SysML2LabeledPropertyGraph,
+    feat: dict,
+) -> int:
+    banded_featuring_graph = lpg.get_projection("expanded_banded_graph")
+    banded_roots = [
+        node
+        for node in banded_featuring_graph.nodes
+        if banded_featuring_graph.out_degree(node) < 1
+    ]
 
     corrected_mult = 1
-
     for part_tree_root in banded_roots:
         try:
-            part_path = NX.shortest_path(
+            part_path = nx.shortest_path(
                 banded_featuring_graph,
-                feat['@id'],
-                part_tree_root)
+                source=feat['@id'],
+                target=part_tree_root,
+            )
             # TODO: check that the path actually exists
-            corrected_mult = math.prod(
-                [feature_upper_multiplicity(client.elements_by_id[node], client)
-                 for node in part_path])
-        except NX.NetworkXNoPath:
+            corrected_mult = math.prod([
+                feature_upper_multiplicity(lpg.elements_by_id[node], lpg)
+                for node in part_path
+            ])
+        except nx.NetworkXNoPath:
             pass
-        except NX.NodeNotFound:
+        except nx.NodeNotFound:
             # nothing to roll up, so just use own multiplicity
-            corrected_mult = feature_lower_multiplicity(feat, client)
+            corrected_mult = feature_upper_multiplicity(feat, lpg)
 
     return corrected_mult
