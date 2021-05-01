f71599b42c6b139c45ab55ccd5a0d91f8c3207ea
bjorncole <bjorn.cole@gmail.com>
2021-05-01 13:54:25 -0400

    Stubbing unit tests for SysML v2 API downloads

---
+++ tests/test_client_downloading.py
@@ -0,0 +1,30 @@
+def test_all_downloaded():
+    """
+    Test that all elements in a given project actually downloaded
+    :return: test result
+    """
+    assert True
+
+
+def test_ele_v_relationship():
+    """
+    Test that elements are correctly partitioned between non-relationship and relationship
+    :return: test result
+    """
+    assert True
+
+
+def test_ele_types():
+    """
+    Test that elements are downloaded with correct metatype
+    :return: test result
+    """
+    assert True
+
+
+def test_eles_populated():
+    """
+    Test that elements are populated with actual data
+    :return: test result
+    """
+    assert True
