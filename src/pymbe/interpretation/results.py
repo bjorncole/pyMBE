dfdf8b5345727a982b0e2e056cb063f9f3ea4947
bjorncole <bjorn.cole@gmail.com>
2021-05-02 09:28:05 -0400

    Added pretty print for id lists

--- src/pymbe/interpretation/results.py
+++ src/pymbe/interpretation/results.py
@@ -1,6 +1,6 @@
 # A set of tools to make interpretations easier to read
 from ..label import get_label
-
+from ..label import get_label_for_id
 
 def pprint_interpretation(interpretation: dict, all_elements: dict) -> list:
     print_lines = []
@@ -14,4 +14,23 @@ def pprint_interpretation(interpretation: dict, all_elements: dict) -> list:
             short_list.append(['..'])
 
         print_lines.extend(short_list)
+    return print_lines
+
+
+def pprint_single_id_list(list_to_print: list, all_elements: dict) -> list:
+    print_lines = []
+    for item in list_to_print:
+        print_lines.append(get_label_for_id(item, all_elements))
+
+    return print_lines
+
+
+def pprint_double_id_list(list_to_print: list, all_elements: dict) -> list:
+    print_lines = []
+    for seq in list_to_print:
+        seq_line = []
+        for item in seq:
+            seq_line.append(get_label_for_id(item, all_elements))
+        print_lines.append(seq_line)
+
     return print_lines
