eb786e9673e9ffd2212028e0dad84911e9dabd47
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2021-07-24 11:29:10 -0400

    Reorganized almost all the notebooks and updated the notebook tests calls

--- tests/jupyter/test_notebooks.py
+++ tests/jupyter/test_notebooks.py
@@ -1,7 +1,7 @@
 from testbook import testbook
 
 
-@testbook("notebooks/Tutorial.ipynb", execute=True)
+@testbook("docs/tutorials/01-Basic.ipynb", execute=True)
 def test_tutorial(tb):
     """Test the Tutorial notebook"""
     client = tb.ref("client")
@@ -14,13 +14,13 @@ def test_tutorial(tb):
     assert not set(diagram.model.elements).symmetric_difference(set(client.model.elements))
 
 
-@testbook("dev_doc/Playbook Explorer Parts Test.ipynb", execute=True)
+@testbook("tests/jupyter/notebooks/Playbook Explorer Parts Test.ipynb", execute=True)
 def test_simple_parts_explorer(tb):
     m0_interpretation = tb.ref("m0_interpretation")
     assert m0_interpretation
 
 
-@testbook("dev_doc/SysML Model.ipynb", execute=True)
+@testbook("docs/how_to_guides/SysML Model.ipynb", execute=True)
 def test_sysml_model(tb):
     kerbal_element = tb.ref("kerbal")
     assert kerbal_element._data["name"] == "Kerbal"
