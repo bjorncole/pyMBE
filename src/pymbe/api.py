a85e4370b33902e6657177def59bcf00c5b72cc9
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2021-07-24 12:52:13 -0400

    Cleaned up the __all__ in api.py

--- src/pymbe/api.py
+++ src/pymbe/api.py
@@ -1,3 +1,14 @@
+__all__ = (
+    "ContainmentTree",
+    "DiagramWidget",
+    "ElementInspector",
+    "Instance",
+    "Model",
+    "SysML2ClientWidget",
+    "UI",
+)
+
+
 from .widget.client import SysML2ClientWidget
 from .widget.containment import ContainmentTree
 from .widget.diagram import DiagramWidget
@@ -5,14 +16,3 @@ from .widget.inspector import ElementInspector
 from .widget.ui import UI
 from .interpretation.interpretation import Instance
 from .model import Model
-
-
-__all__ = (
-    Model,
-    Instance,
-    UI,
-    ElementInspector,
-    DiagramWidget,
-    ContainmentTree,
-    SysML2ClientWidget,
-)
