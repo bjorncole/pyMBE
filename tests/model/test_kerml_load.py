2ff757d1163e743f8947ba9ff94cee5a15a7727c
bjorncole <bjorn.cole@gmail.com>
2023-04-30 10:03:07 -0400

    Added feature checks to basic model load tests

--- tests/model/test_kerml_load.py
+++ tests/model/test_kerml_load.py
@@ -36,27 +36,46 @@ def test_classifier_load(basic_load_files):
 
     assert "Adding Machine" in [classifier.declaredName for classifier in level3_classes]
 
-    assert True
-
 def test_feature_load(basic_load_files):
 
+    level1 = basic_load_files["Level1"]
+    level2 = basic_load_files["Level2"]
+    level3 = basic_load_files["Level3"]
+
     # Level 1 check
+
+    level1_features = [feature for feature in list(level1.elements.values()) if feature._metatype == 'Feature']
+
     # find Test Feature 1
+    assert "Test Feature 1" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
     # find Test Feature 2
+    assert "Test Feature 2" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
     # find Bare Feature
+    assert "Bare Feature" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
     # find Bare Classed Feature
+    assert "Bare Classed Feature" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
     # find Typed Feature 1
+    assert "Typed Feature 1" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
     # find Typed Feature 2
+    assert "Typed Feature 2" in [feature.declaredName for feature in level1_features if hasattr(feature, "declaredName")]
 
     # Level 2 check
+    level2_features = [feature for feature in list(level2.elements.values()) if feature._metatype == 'Feature']
     # find Side 1
+    assert "Side 1" in [feature.declaredName for feature in level2_features  if hasattr(feature, "declaredName")]
     # find two 'Value' features
+    assert "Value" in [feature.declaredName for feature in level2_features  if hasattr(feature, "declaredName")]
+    assert len([feature for feature in level2_features
+                 if hasattr(feature, "declaredName") and feature.declaredName == "Value"]) == 2
     # find Side 2
+    assert "Side 2" in [feature.declaredName for feature in level2_features  if hasattr(feature, "declaredName")]
 
     # Level 3 check
+    level3_features = [feature for feature in list(level3.elements.values()) if feature._metatype == 'Feature']
     # find Register 1
+    assert "Register 1" in [feature.declaredName for feature in level3_features  if hasattr(feature, "declaredName")]
     # find Register 2
+    assert "Register 2" in [feature.declaredName for feature in level3_features  if hasattr(feature, "declaredName")]
     # find Register 3
+    assert "Register 3" in [feature.declaredName for feature in level3_features  if hasattr(feature, "declaredName")]
     
-
-    assert True
