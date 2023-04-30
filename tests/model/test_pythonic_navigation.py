e63b15a9b3b7319f9b38e26cc1d37625f470c23e
bjorncole <bjorn.cole@gmail.com>
2023-04-30 16:56:43 -0400

    Added in tests for expressions

---
+++ tests/model/test_pythonic_navigation.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pymbe.model import Element, Model
+
+def test_pythonic_attributes(basic_load_files):
+    '''
+    Test that primary attributes are loaded into elements such that they can be referenced as normal Python prpperties
+    '''
+
+    assert True
+
+def test_pythonic_references(basic_load_files):
+    '''
+    Test that primary references are loaded into elements such that they can be referenced as normal Python prpperties
+    '''
+
+    assert True
+
+def test_pythonic_through_reverse(basic_load_files):
+    '''
+    Test that 'through' and 'reverse' Pythonic properties work correctly
+    '''
+
+    assert True
